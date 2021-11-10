e9c3021a31bf21968ff3e436263f951e4aca4fa6
adelkuanysheva <77294028+adelkuanysheva@users.noreply.github.com>
2021-11-09 16:23:26 -0800

    Implement unsplash fetch functionality

--- controller/auth_controller.js
+++ controller/auth_controller.js
@@ -9,15 +9,31 @@ let authController = {
     res.render("auth/register");
   },
 
-  registerSubmit: (req, res, next) => {
+  registerSubmit: async (req, res, next) => {
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
-    userModel.addOne(email, password, name);
-    
+    const clientId = process.env.UNSPLASH_ID
+    const query = "quokka";
+
+
+    const url = `https://api.unsplash.com/photos?client_id=${clientId}&query=${query}`
+
+    // AWAIT
+    // const data = await fetch(url);
+    // const newData = await data.json();
+    // const imageFromUnsplash = newData.results;
+    // userModel.addOne(email, password, name, imageFromUnsplash)
+
+    fetch(url)
+    .then((data) => data.json()) 
+    .then((newData) => {
+      const imageFromUnsplash = newData.results;
+    })
+    .then((imageFromUnsplash) => userModel.addOne(email, password, name, imageFromUnsplash))
+    .catch((err) => console.log(err)),
     next();
   },
-
   home: (req, res) => {
     res.render('./reminder/dashboard', { user: req.user.username });
   }, 
