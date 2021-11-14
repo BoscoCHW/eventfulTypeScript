88a614ceff55630e774ab899a3a9bb54b3225e8c
Bosco_Acer <bosco.chw@gmail.com>
2021-11-13 23:59:25 -0800

    Put /uploads route in the file indexRoute.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const upload = require("./middleware/multer")
+
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 const ejsLayouts = require("express-ejs-layouts");
 const session = require('express-session');
 const passport = require("./middleware/passport");
-const imgur = require("imgur");
-const fs = require("fs");
-const database = require("./database");
 
 
 require("dotenv").config()
@@ -41,36 +38,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use((req, res, next) => {
-  console.log(req.user)
-  // console.log(`=================================================`);
-  next();
-});
-
 
 const indexRoute = require("./routes/indexRoute");
 const authRoute = require("./routes/authRoute");
 app.use("/", indexRoute);
 app.use("/auth", authRoute);
 
-
-
-app.post("/uploads/", upload.any(), async (req, res) => {
-  const file = req.files[0];
-  try {
-    const url = await imgur.uploadFile(`./uploads/${file.filename}`);
-    database.forEach(function(obj) {
-      if (obj.id === req.user.id) {
-          obj.imageUrl = url.link;
-          console.log(obj)
-      }
-  });
-    res.json({ message: url.link });
-    fs.unlinkSync(`./uploads/${file.filename}`);
-  } catch (error) {
-    console.log("error", error);
-  }
-});
 
 app.listen(3001, function () {
   console.log(
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -4,6 +4,8 @@ const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
 const reminderController = require("../controller/reminder_controller");
 const imgur = require("imgur");
 const fs = require("fs");
+const upload = require("../middleware/multer")
+const database = require("../database")
 
 router.get("/reminders", ensureAuthenticated, reminderController.list);
 
@@ -40,5 +42,21 @@ router.get("/revokeSession/:id", ensureAuthenticated, isAdmin, (req, res) => {
     delete req.sessionStore.sessions[sessionId]
     return res.redirect("/admin")
 })
+
+router.post("/uploads/", upload.single("image"), async (req, res) => {
+    const file = req.file;
+    try {
+        const url = await imgur.uploadFile(`./uploads/${file.filename}`);
+        database.forEach(user => {
+            if (user.id === req.user.id) {
+                user.imageUrl = url.link;
+            }
+        });
+        res.json({ message: url.link });
+        fs.unlinkSync(`./uploads/${file.filename}`);
+    } catch (error) {
+        console.log("error", error);
+    }
+  });
 
 module.exports = router;
